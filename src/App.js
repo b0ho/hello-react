e0f43dece2c1489216028cdda297e7de7990e156
b0ho <ybg4828@gmail.com>
2021-10-14 23:20:34 +0900

    state test2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import "./App.css";
 import MyComponent from "./MyComponent";
 import Counter from "./Counter";
-import { Fragment } from "react/cjs/react.production.min";
 import Say from "./Say";
 
 const App = () => {
   return (
-    <Fragment>
+    <div>
       <MyComponent name="React" favoriteNumber="s">
         리액트
       </MyComponent>
       <Counter></Counter>
       <Say></Say>
-    </Fragment>
+    </div>
   );
 };
 
