7c4e1912240644ac909d764b94831852e787f086
b0ho <ybg4828@gmail.com>
2021-10-17 18:08:01 +0900

    ref 테스트

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from "./Counter";
 import Say from "./Say";
 import Event from "./Event";
 import EventFunc from "./EventFunc";
+import Validation from "./Validation";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <MyComponent name="React" favoriteNumber="s">
         리액트
       </MyComponent>
+      <Validation></Validation>
       <EventFunc></EventFunc>
       <Counter></Counter>
       <Say></Say>
