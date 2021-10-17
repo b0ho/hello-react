2237c783379cf256af56edae13555232def1741e
b0ho <ybg4828@gmail.com>
2021-10-17 17:48:00 +0900

    함수형 이벤트 테스트2
    단일 이벤트로 처리
    비구조화 할당 사용

--- src/EventFunc.js
+++ src/EventFunc.js
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 
 const EventFunc = () => {
-  const [username, setUsername] = useState("");
-  const [message, setMessage] = useState("");
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: "",
+    message: "",
+  });
+  const { username, message } = form;
+
+  const onChange = (e) => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+    setForm(nextForm);
+  };
+
   const onClick = () => {
     alert(username + ":" + message);
-    setUsername("");
-    setMessage("");
+    setForm({
+      username: "",
+      message: "",
+    });
   };
+
   const onKeyPress = (e) => {
     if (e.key === "Enter") {
       onClick();
     }
   };
+
   return (
     <div>
       <h1>함수형 이벤트</h1>
@@ -23,14 +37,14 @@ const EventFunc = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="입력하세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
