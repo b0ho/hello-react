3f0e3bfec3d72da34d201b2c6fb7e5eb0810d59a
b0ho <ybg4828@gmail.com>
2021-10-19 00:13:04 +0900

    iteration 테스트2
    아이템 추가 이벤트
    아이템 삭제 이벤트

--- src/Iteration.js
+++ src/Iteration.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Iteration = () => {
-  const names = ["a", "b", "c", "d"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: "A" },
+    { id: 2, text: "B" },
+    { id: 3, text: "C" },
+    { id: 4, text: "D" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
   return (
     <div>
-      <ul>
-        <li> A </li>
-        <li> B </li>
-        <li> C </li>
-        <li> D </li>
-      </ul>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
       <ul>{nameList}</ul>
     </div>
   );
