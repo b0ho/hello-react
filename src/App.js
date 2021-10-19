0594c28e558815eda5f3d67a4f8e4cb20adaf710
b0ho <ybg4828@gmail.com>
2021-10-19 23:41:17 +0900

    lifecycle 테스트

--- src/App.js
+++ src/App.js
@@ -9,11 +9,30 @@ import Validation from "./Validation";
 import Ref from "./Ref";
 import ScrollBox from "./ScrollBox";
 import Iteration from "./Iteration";
+import LifeCycle from "./LifeCycle";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
+        <div>
+          <button onClick={this.handleClick}>랜덤색상</button>
+          <LifeCycle color={this.state.color}></LifeCycle>
+        </div>
         <MyComponent name="React" favoriteNumber="s">
           리액트
         </MyComponent>
